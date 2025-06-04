a563cc0d8e6267de3c77529b7d567d5686af88cc
fazlecse <mdfazlerabbi06@gmail.com>
2025-06-05 02:10:46 +0600

    Admin Edit Product

--- resources/views/admin/product-edit.blade.php
+++ resources/views/admin/product-edit.blade.php
@@ -4,7 +4,7 @@
     <!-- main-content-wrap -->
     <div class="main-content-wrap">
       <div class="flex items-center flex-wrap justify-between gap20 mb-27">
-        <h3>Add Product</h3>
+        <h3>Edit Product</h3>
         <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
           <li>
             <a href="index-2.html">
@@ -23,21 +23,23 @@
             <i class="icon-chevron-right"></i>
           </li>
           <li>
-            <div class="text-tiny">Add product</div>
+            <div class="text-tiny">Edit product</div>
           </li>
         </ul>
       </div>
-      <!-- form-add-product -->
-      <form class="tf-section-2 form-add-product" method="POST" enctype="multipart/form-data"
-        action="{{ route('admin.product.store') }}">
+      <!-- form-update-product -->
+      <form class="tf-section-2 form-update-product" method="POST" enctype="multipart/form-data"
+        action="{{ route('admin.product.update') }}">
         @csrf
+        @method('PUT')
+        <input type="hidden" name="id" value="{{ $product->id }}">
         <div class="wg-box">
           <fieldset class="name">
             <div class="body-title mb-10">Product name
               <span class="tf-color-1">*</span>
             </div>
             <input class="mb-10" type="text" placeholder="Enter product name" name="name" tabindex="0"
-              value="{{ old('name') }}" aria-required="true" required="">
+              value="{{ $product->name }}" aria-required="true" required="">
             <div class="text-tiny">Do not exceed 100 characters when entering the
               product name.</div>
           </fieldset>
@@ -48,7 +50,7 @@
           <fieldset class="name">
             <div class="body-title mb-10">Slug <span class="tf-color-1">*</span></div>
             <input class="mb-10" type="text" placeholder="Enter product slug" name="slug" tabindex="0"
-              value="{{ old('slug') }}" aria-required="true" required="">
+              value="{{ $product->slug }}" aria-required="true" required="">
             <div class="text-tiny">Do not exceed 100 characters when entering the
               product name.</div>
           </fieldset>
@@ -64,7 +66,8 @@
                 <select class="" name="category_id">
                   <option>Choose category</option>
                   @foreach ($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    <option value="{{ $category->id }}" {{ $product->category_id == $category->id ? 'selected' : '' }}>
+                      {{ $category->name }}</option>
                   @endforeach
                 </select>
               </div>
@@ -80,7 +83,8 @@
                 <select class="" name="brand_id">
                   <option>Choose Brand</option>
                   @foreach ($brands as $brand)
-                    <option value="{{ $brand->id }}">{{ $brand->name }}</option>
+                    <option value="{{ $brand->id }}" {{ $product->brand_id == $brand->id ? 'selected' : '' }}>
+                      {{ $brand->name }}</option>
                   @endforeach
                 </select>
               </div>
@@ -93,7 +97,7 @@
           <fieldset class="shortdescription">
             <div class="body-title mb-10">Short Description <span class="tf-color-1">*</span></div>
             <textarea class="mb-10 ht-150" name="short_description" placeholder="Short Description" tabindex="0"
-              aria-required="true" required="">{{ old('short_description') }}</textarea>
+              aria-required="true" required="">{{ $product->short_description }}</textarea>
             <div class="text-tiny">Do not exceed 100 characters when entering the
               product name.</div>
           </fieldset>
@@ -105,7 +109,7 @@
             <div class="body-title mb-10">Description <span class="tf-color-1">*</span>
             </div>
             <textarea class="mb-10" name="description" placeholder="Description" tabindex="0" aria-required="true"
-              required="">{{ old('description') }}</textarea>
+              required="">{{ $product->description }}</textarea>
             <div class="text-tiny">Do not exceed 100 characters when entering the
               product name.</div>
           </fieldset>
@@ -115,12 +119,14 @@
         </div>
         <div class="wg-box">
           <fieldset>
-            <div class="body-title">Upload images <span class="tf-color-1">*</span>
+            <div class="body-title mb-10">Upload images <span class="tf-color-1">*</span>
             </div>
             <div class="upload-image flex-grow">
-              <div class="item" id="imgpreview" style="display:none">
-                <img src="../../../localhost_8000/images/upload/upload-1.png" class="effect8" alt="">
-              </div>
+              @if ($product->image)
+                <div class="item" id="imgpreview">
+                  <img src="{{ asset('uploads/products') }}/{{ $product->image }}" class="effect8" alt="">
+                </div>
+              @endif
               <div id="upload-file" class="item up-load">
                 <label class="uploadfile" for="myFile">
                   <span class="icon">
@@ -140,9 +146,13 @@
           <fieldset>
             <div class="body-title mb-10">Upload Gallery Images</div>
             <div class="upload-image mb-16">
-              {{-- <div class="item">
-                <img src="images/upload/upload-1.png" alt="">
-              </div> --}}
+              @if ($product->images)
+                @foreach (explode(',', $product->images) as $img)
+                  <div class="item gitems">
+                    <img src="{{ asset('uploads/products') }}/{{ trim($img) }}" alt="">
+                  </div>
+                @endforeach
+              @endif
               <div id="galUpload" class="item up-load">
                 <label class="uploadfile" for="gFile">
                   <span class="icon">
@@ -163,7 +173,7 @@
             <fieldset class="name">
               <div class="body-title mb-10">Regular Price <span class="tf-color-1">*</span></div>
               <input class="mb-10" type="text" placeholder="Enter regular price" name="regular_price"
-                tabindex="0" value="{{ old('regular_price') }}" aria-required="true" required="">
+                tabindex="0" value="{{ $product->regular_price }}" aria-required="true" required="">
             </fieldset>
             @error('regular_price')
               <span class="alert alert-danger text-center">{{ $message }}</span>
@@ -172,7 +182,7 @@
             <fieldset class="name">
               <div class="body-title mb-10">Sale Price <span class="tf-color-1">*</span></div>
               <input class="mb-10" type="text" placeholder="Enter sale price" name="sale_price" tabindex="0"
-                value="{{ old('sale_price') }}" aria-required="true" required="">
+                value="{{ $product->sale_price }}" aria-required="true" required="">
             </fieldset>
             @error('sale_price')
               <span class="alert alert-danger text-center">{{ $message }}</span>
@@ -184,7 +194,7 @@
               <div class="body-title mb-10">SKU <span class="tf-color-1">*</span>
               </div>
               <input class="mb-10" type="text" placeholder="Enter SKU" name="SKU" tabindex="0"
-                value="{{ old('SKU') }}" aria-required="true" required="">
+                value="{{ $product->SKU }}" aria-required="true" required="">
             </fieldset>
             @error('SKU')
               <span class="alert alert-danger text-center">{{ $message }}</span>
@@ -194,7 +204,7 @@
               <div class="body-title mb-10">Quantity <span class="tf-color-1">*</span>
               </div>
               <input class="mb-10" type="text" placeholder="Enter quantity" name="quantity" tabindex="0"
-                value="{{ old('quantity') }}" aria-required="true" required="">
+                value="{{ $product->quantity }}" aria-required="true" required="">
             </fieldset>
             @error('quantity')
               <span class="alert alert-danger text-center">{{ $message }}</span>
@@ -206,8 +216,10 @@
               <div class="body-title mb-10">Stock</div>
               <div class="select mb-10">
                 <select class="" name="stock_status">
-                  <option value="instock">InStock</option>
-                  <option value="outofstock">Out of Stock</option>
+                  <option value="instock" {{ $product->stock_status == 'instock' ? 'selected' : '' }}>InStock
+                  </option>
+                  <option value="outofstock" {{ $product->stock_status == 'outofstock' ? 'selected' : '' }}>Out
+                    of Stock</option>
                 </select>
               </div>
             </fieldset>
@@ -218,8 +230,8 @@
               <div class="body-title mb-10">Featured</div>
               <div class="select mb-10">
                 <select class="" name="featured">
-                  <option value="0">No</option>
-                  <option value="1">Yes</option>
+                  <option value="0" {{ $product->featured == '0' ? 'selected' : '' }}>No</option>
+                  <option value="1" {{ $product->featured == '1' ? 'selected' : '' }}>Yes</option>
                 </select>
               </div>
             </fieldset>
@@ -228,11 +240,11 @@
             @enderror
           </div>
           <div class="cols gap10">
-            <button class="tf-button w-full" type="submit">Add product</button>
+            <button class="tf-button w-full" type="submit">Update product</button>
           </div>
         </div>
       </form>
-      <!-- /form-add-product -->
+      <!-- /form-update-product -->
     </div>
     <!-- /main-content-wrap -->
   </div>
